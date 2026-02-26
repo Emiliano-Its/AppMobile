babc331a8569d9263a855a1e10d1f81235da3609
Emiliano-Its <emiliano.jecr@gmail.com>
2026-02-26 16:45:26 -0600

    Some implementations in the back

--- backend/sales/models.py
+++ backend/sales/models.py
@@ -10,6 +10,7 @@ class FinalProduct(models.Model):
     nombre = models.CharField(max_length=100)
     precio_venta = models.DecimalField(max_digits=10, decimal_places=2)
     stock_actual = models.IntegerField(default=0)
+    activo = models.BooleanField(default=True)
 
     def __str__(self):
         return self.nombre
--- backend/sales/views.py
+++ backend/sales/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from rest_framework.response import Response
 from .models import Venta,FinalProduct
 from .serializers import FinalProductSerializer, SaleSerializer
@@ -7,6 +8,13 @@ from .serializers import FinalProductSerializer, SaleSerializer
 class FinalProductViewSet(viewsets.ModelViewSet):
     queryset = FinalProduct.objects.all()
     serializer_class = FinalProductSerializer
+
+    @action(detail=True, methods=['post'])
+    def toggle_active(self, request, pk=None):
+        product = self.get_object()
+        product.activo = not product.activo
+        product.save()
+        return Response({'status': 'producto actualizado', 'activo': product.activo})
 
 class SaleViewSet(viewsets.ModelViewSet):
     queryset = Venta.objects.all().order_by('-fecha')
@@ -19,6 +27,30 @@ class SaleViewSet(viewsets.ModelViewSet):
         if sale_type:
             queryset = queryset.filter(tipo=sale_type)
         return queryset
+    
+    @action(detail=True, methods=['post'])
+    def completar_pedido(self, request, pk=None):
+        venta = self.get_object()
+        
+        if venta.tipo != "PEDIDO":
+            return Response({'error': 'Esta venta ya fue procesada'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # 1. Recorrer los detalles y restar stock
+        for detalle in venta.detalles.all():
+            producto = detalle.producto
+            if producto.stock_actual < detalle.cantidad:
+                return Response({
+                    'error': f'Stock insuficiente para {producto.nombre}'
+                }, status=status.HTTP_400_BAD_REQUEST)
+            
+            producto.stock_actual -= detalle.cantidad
+            producto.save()
+
+        # 2. Cambiar el tipo a LOCAL (o venta finalizada)
+        venta.tipo = "LOCAL" 
+        venta.save()
+
+        return Response({'status': 'Pedido completado y stock actualizado'})
 
 class POSProductViewSet(viewsets.ReadOnlyModelViewSet):
     """ Viewset for the POS to search products quickly """
